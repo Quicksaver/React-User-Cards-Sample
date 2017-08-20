37f43400eee59506ffc932ac546be9320e5e7359
Luís Miguel <quicksaver@gmail.com>
2017-08-20 22:28:38 +0100

    nit: re-set tab indentation on files I'll use most

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App"/>
-    );
-  }
+    render() {
+        return (
+            <div className="App"/>
+        );
+    }
 }
 
 export default App;
