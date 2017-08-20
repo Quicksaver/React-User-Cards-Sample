8a796b790823b561d6f6d610b9ad5805ae761224
Luís Miguel <quicksaver@gmail.com>
2017-08-20 23:16:00 +0100

    Comment with a few references I forgot to mention

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+// Learning refs:
+//   https://medium.com/@diamondgfx/learning-react-with-create-react-app-part-1-a12e1833fdc
+//   https://facebook.github.io/react/docs/hello-world.html
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
